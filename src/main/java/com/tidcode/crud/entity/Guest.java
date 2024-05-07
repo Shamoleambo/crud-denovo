a5ddd577b1466f0116138ad8d7e83c8715565086
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-05-07 16:21:29 -0300

    create Guest entity

---
+++ src/main/java/com/tidcode/crud/entity/Guest.java
@@ -0,0 +1,72 @@
+package com.tidcode.crud.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Guest {
+
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column
+	private String firstName;
+	@Column
+	private String lastName;
+	@Column
+	private Boolean confirmed;
+	@Column
+	private String email;
+
+	public Guest(String firstName, String lastName, Boolean confirmed, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.confirmed = confirmed;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Boolean getConfirmed() {
+		return confirmed;
+	}
+
+	public void setConfirmed(Boolean confirmed) {
+		this.confirmed = confirmed;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
