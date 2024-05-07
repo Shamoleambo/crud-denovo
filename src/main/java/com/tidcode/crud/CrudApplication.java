bf108c16b52bc9c751599efd94bda076eec74b56
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-05-07 17:36:15 -0300

    add multiple guests for test purposes

--- src/main/java/com/tidcode/crud/CrudApplication.java
+++ src/main/java/com/tidcode/crud/CrudApplication.java
@@ -21,7 +21,8 @@ public class CrudApplication {
 	public CommandLineRunner run(GuestDAO guestDAO) {
 		return args -> {
 //			findAllGuests(guestDAO);
-			addGuest(guestDAO);
+//			addGuest(guestDAO);
+			addMultipleGuests(guestDAO);
 		};
 	}
 
@@ -32,10 +33,24 @@ public class CrudApplication {
 			System.out.println(guest);
 		}
 	}
-	
+
 	private void addGuest(GuestDAO guestDAO) {
 		Guest newGuest = new Guest("Mano", "Silva", true, "mano@mail.com");
 		Guest mano = guestDAO.save(newGuest);
 		System.out.println(mano);
+	}
+
+	private void addMultipleGuests(GuestDAO guestDAO) {
+		Guest fulano = new Guest("Fulano", "Dahora", true, "fulano@mail.com");
+		Guest beltrano = new Guest("Beltrano", "Maneiro", true, "beltrano@mail.com");
+		Guest sicrano = new Guest("Sicrano", "Legal", true, "sicrano@mail.com");
+
+		Guest ful = guestDAO.save(fulano);
+		Guest bel = guestDAO.save(beltrano);
+		Guest sic = guestDAO.save(sicrano);
+
+		System.out.println(ful);
+		System.out.println(bel);
+		System.out.println(sic);
 	}
 }
